3b78c87bcf4e81c8d03b3711c98f2d35da0fbde4
Kerem Şenses <68199514+exkali77@users.noreply.github.com>
2023-10-29 10:56:05 +0300

    c# ile uçak bileti
    C# ile kullanıcı uçak bileti alma programı

---
+++ Ucuc rezervasyon/Ucuc rezervasyon/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ucuc_rezervasyon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int ip;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int a=Convert.ToInt32(maskedTextBox1.Text);
+           if(ip==a)
+            {
+                label8.Text = textBox1.Text;
+                label7.Text = textBox2.Text;
+                label6.Text = dateTimePicker1.Text;
+                label5.Text = comboBox1.Text;
+                listBox1.Items.Clear();
+            }
+           else
+            {
+                MessageBox.Show("Girilen Biletnumarası Tanımlı Değil");
+
+            }
+           
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            Random rnd = new Random();
+            ip = rnd.Next(1,35);
+
+            listBox1.Items.Add("Bilet Numarası :" + ip.ToString() + "   Ad Soyad :" + textBox1.Text + "    Eposta :" + textBox2.Text + "    Kart Numarası :" + textBox3.Text + "    SonKulanım Tarihi:" + textBox4.Text  +  "  CVV :"+textBox5.Text + "Uçuş Yeri : " + comboBox1.Text  + "   Bilet Alım Tarihi :"+ dateTimePicker1.Text);
+            
+        }
+
+        
+    }
+}
